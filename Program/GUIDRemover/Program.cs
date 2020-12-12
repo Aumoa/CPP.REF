7986e8940651d5776bc06d40563587afc06765b2
장호철 <wkdghcjf1234@naver.com>
2020-12-13 06:22:10 +0900

    GUIDRemover 프로그램 추가

---
+++ Program/GUIDRemover/Program.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2020 Aumoa.lib. All right reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+List<string> FilterFiles(string[] files)
+{
+    var filtered = new List<string>();
+    filtered.Capacity = files.Length;
+
+    foreach (var file in files)
+    {
+        if (file.EndsWith(".vcxproj"))
+        {
+            filtered.Add(file);
+        }
+    }
+
+    return filtered;
+}
+
+void RemoveGUID(string file)
+{
+    string text = File.ReadAllText(file);
+    int guidStart = text.IndexOf("<ProjectGuid>");
+    if (guidStart == -1)
+    {
+        return;
+    }
+
+    int guidEnd = text.IndexOf("</ProjectGuid>");
+    if (guidEnd == -1)
+    {
+        return;
+    }
+
+    string left = text.Substring(0, guidStart);
+    string right = text.Substring(guidEnd + "</ProjectGuid>".Length);
+
+    string final = left + right;
+    var stream = File.CreateText(file);
+    stream.Write(final);
+    stream.Flush();
+    stream.Close();
+
+    Console.WriteLine($"{file} Cleared.");
+}
+
+Console.WriteLine($"Clear project guid in {args[0]} directory.");
+
+var files = Directory.GetFiles(args[0], "*.*", SearchOption.AllDirectories);
+var vcprojs = FilterFiles(files);
+foreach (var file in vcprojs)
+{
+    RemoveGUID(file);
+}
--- generate_projects.bat
+++ generate_projects.bat
@@ -4,10 +4,17 @@ cd ThirdParty
 
 cd assimp
 rmdir /s /q BINARIES
-start generate_projects.bat
+call generate_projects.bat
 cd ..
 
 cd PhysX\physx\
 rmdir /s /q compiler\vc16win64
-start generate_projects.bat
-cd ..\..
+call generate_projects.bat
+cd ../..
+
+:: Exit from ThirdParty
+cd ..
+
+call Build\Program\net5.0\GUIDRemover.exe "ThirdParty"
+
+pause
