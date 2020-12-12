5cec44ea7917e73be69bbb1b1c9512d567e1a9f7
장호철 <wkdghcjf1234@naver.com>
2020-12-13 04:30:02 +0900

    서드파티 전체 프로젝트 생성 업데이트

--- ThirdParty/generate_projects.bat
+++ ThirdParty/generate_projects.bat
@@ -1,9 +1,11 @@
 :: Copyright 2020 Aumoa.lib. All right reserved.
 
 cd assimp
+rmdir /s /q BINARIES
 start generate_projects.bat
 cd ..
 
 cd PhysX\physx\
+rmdir /s /q compiler\vc16win64
 start generate_projects.bat
 cd ..\..
