9e701dd299c1139c5b63d88ebebe1e3619c089dd
liberty <liberty@clover.games>
2020-12-22 13:15:34 +0900

    glfw 프로젝트 추가

--- generate_projects.bat
+++ generate_projects.bat
@@ -4,10 +4,18 @@
 
 cd ThirdParty
 
+echo -
+echo ---------- cmake assimp ----------
+echo -
+
 cd assimp
 if exist BINARIES rmdir /s /q BINARIES
 call generate_projects.bat
 cd ..
+
+echo -
+echo ---------- cmake PhysX ----------
+echo -
 
 cd PhysX\physx\
 cd compiler
@@ -15,6 +23,15 @@ if exist vc16win64 rmdir /s /q vc16win64
 cd ..
 call generate_projects.bat
 cd ../..
+
+echo -
+echo ---------- cmake glfw ----------
+echo -
+
+cd glfw
+if exist BINARIES rmdir /s /q BINARIES
+call generate_projects.bat
+cd ..
 
 :: Exit from ThirdParty
 cd ..
