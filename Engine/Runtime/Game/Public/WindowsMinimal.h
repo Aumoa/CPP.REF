16a1adb5cae76f152e61efc861ced0122decfeb7
liberty <liberty@clover.games>
2020-12-24 01:34:01 +0900

    외부 구성 요소에서 Windows 헤더에 접근하지 않게

--- Engine/Runtime/Game/Public/WindowsMinimal.h
+++ Engine/Runtime/Game/Public/WindowsMinimal.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#ifdef __SC_RUNTIME_GAME_API__
+
 #undef interface
 
 #define WIN32_LEAN_AND_MEAN
@@ -23,3 +25,32 @@
 #endif
 
 #undef CreateWindow
+
+#else
+
+#ifndef CALLBACK
+#define CALLBACK __stdcall
+#endif
+
+#ifndef WINAPI
+#define WINAPI __stdcall
+#endif
+
+#ifndef _In_
+#define _In_
+#endif
+
+#ifndef _In_opt_
+#define _In_opt_
+#endif
+
+using HWND = void*;
+using UINT = uint32;
+using WPARAM = uint64;
+using LPARAM = int64;
+using LRESULT = int64;
+using INT = int32;
+using HINSTANCE = void*;
+using LPSTR = char*;
+
+#endif
