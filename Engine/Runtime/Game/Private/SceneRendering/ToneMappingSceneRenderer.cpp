0832cb82860e992de667ea2ba2ee106571a6ab97
liberty <liberty@clover.games>
2021-03-08 23:26:49 +0900

    SceneRenderer 프로토타입 추가

---
+++ Engine/Runtime/Game/Private/SceneRendering/ToneMappingSceneRenderer.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#include "SceneRendering/ToneMappingSceneRenderer.h"
+
+ToneMappingSceneRenderer::ToneMappingSceneRenderer(IRHIScene* scene) : Super(scene)
+{
+
+}
+
+ToneMappingSceneRenderer::~ToneMappingSceneRenderer()
+{
+
+}
+
+void ToneMappingSceneRenderer::RenderScene(IRHICommandList* immediateCommandList)
+{
+
+}
