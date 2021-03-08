3367880591e67d5e7145c071c92936b2f96f836f
liberty <liberty@clover.games>
2021-03-08 23:24:40 +0900

    SBT 프로토타입 추가

---
+++ Engine/Runtime/Game/Private/DirectX/DirectXShaderBindingTable.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#include "DirectX/DirectXShaderBindingTable.h"
+
+#include "DirectX/DirectXCommon.h"
+#include "DirectX/DirectXDeviceBundle.h"
+
+DirectXShaderBindingTable::DirectXShaderBindingTable(DirectXDeviceBundle* deviceBundle) : Super()
+	, device(deviceBundle->GetDevice())
+{
+	rayGeneration = CreateBuffer(D3D12_SHADER_IDENTIFIER_SIZE_IN_BYTES);
+}
+
+DirectXShaderBindingTable::~DirectXShaderBindingTable()
+{
+
+}
+
+TComPtr<ID3D12Resource> DirectXShaderBindingTable::CreateBuffer(uint64 sizeInBytes)
+{
+	D3D12_RESOURCE_DESC bufferDesc = { };
+	bufferDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	bufferDesc.Width = sizeInBytes;
+	bufferDesc.Height = 1;
+	bufferDesc.DepthOrArraySize = 1;
+	bufferDesc.MipLevels = 1;
+	bufferDesc.SampleDesc = { 1, 0 };
+	bufferDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	D3D12_HEAP_PROPERTIES heapProp = { };
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	TComPtr<ID3D12Resource> resource;
+	HR(device->CreateCommittedResource(&heapProp, D3D12_HEAP_FLAG_NONE, &bufferDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&resource)));
+
+	return resource;
+}
---
+++ Engine/Runtime/Game/Public/DirectX/DirectXShaderBindingTable.h
@@ -0,0 +1,26 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#pragma once
+
+#include "GameAPI.h"
+#include "CoreMinimal.h"
+#include "DirectXDeviceResource.h"
+
+#include "DirectXMinimal.h"
+
+class GAME_API DirectXShaderBindingTable : public DirectXDeviceResource
+{
+public:
+	using Super = DirectXDeviceResource;
+
+private:
+	ID3D12Device5* device;
+	TComPtr<ID3D12Resource> rayGeneration;
+
+public:
+	DirectXShaderBindingTable(DirectXDeviceBundle* deviceBundle);
+	~DirectXShaderBindingTable() override;
+
+private:
+	TComPtr<ID3D12Resource> CreateBuffer(uint64 sizeInBytes);
+};
