bac768004c706ba0922269a7938df649b5356671
liberty <liberty@clover.games>
2021-03-15 22:53:38 +0900

    IDisposable 패턴 추가

---
+++ Engine/Runtime/Core/Public/IDisposable.h
@@ -0,0 +1,11 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#pragma once
+
+#include "CoreAPI.h"
+#include "Object.h"
+
+interface IDisposable : virtual public Object
+{
+	virtual void Dispose() = 0;
+};
