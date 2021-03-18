1f5d0c5e5d34f157746a90c91e52d8a634c5eb77
liberty <liberty@clover.games>
2021-03-19 01:06:05 +0900

    인스턴싱된 버퍼 할당기 추가

--- Engine/Runtime/Game/Public/DirectX/DirectXInstancedBufferAllocator.h
+++ Engine/Runtime/Game/Public/DirectX/DirectXInstancedBufferAllocator.h
@@ -14,15 +14,32 @@ public:
 	using Super = DirectXDeviceResource;
 
 private:
-	const bool bDynamic : 1;
+	DirectXDeviceBundle* deviceBundle;
 	TComPtr<ID3D12Resource> resource;
+	void* bufferPtr;
 	size_t count;
 	size_t capacity;
 
 public:
-	DirectXInstancedBufferAllocator(DirectXDeviceBundle* deviceBundle, bool bDynamic = true);
+	DirectXInstancedBufferAllocator(DirectXDeviceBundle* deviceBundle, size_t itemSize);
 	~DirectXInstancedBufferAllocator() override;
+
+	void ShrinkToFit();
+	uint64 GetGPUVirtualAddress() const;
+
+	int8& Get(size_t index);
+	const int8& Get(size_t index) const;
+	template<class T, class... TConstructorArgs>
+	T& Emplace(size_t index, TConstructorArgs&&... inArgs);
 
 	vs_property(size_t, Count);
 	vs_property_get_auto(size_t, Capacity, capacity);
+	vs_property_get_auto(ID3D12Resource*, Resource, resource.Get());
+	const size_t ItemSize;
+	const bool bVolatile : 1;
+
+private:
+	void SetCapacity(size_t newCapacity);
 };
+
+#include "DirectXInstancedBufferAllocator.inl"
