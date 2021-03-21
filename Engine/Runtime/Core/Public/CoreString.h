c1669933c71cfe60759d218a5dbde76bf53c88ea
liberty <liberty@clover.games>
2021-03-21 12:32:41 +0900

    String.IndexOf 함수 추가
    String.IndexOf 함수 추가
    
    IndexOfAny 함수 추가

--- Engine/Runtime/Core/Public/CoreString.h
+++ Engine/Runtime/Core/Public/CoreString.h
@@ -77,6 +77,8 @@ public:
 	virtual ConstIterator cend() const override;
 
 	TRefPtr<String> Substring(size_t startIndex, std::optional<size_t> length = std::nullopt) const;
+	std::optional<size_t> IndexOf(TRefPtr<String> value, size_t startIndex = 0, bool bIgnoreCase = false) const;
+	std::optional<size_t> IndexOf(wchar_t value, size_t startIndex = 0, bool bIgnoreCase = false) const;
 
 	vs_property_get(const wchar_t*, C_Str);
 	vs_property_get(size_t, Length);
