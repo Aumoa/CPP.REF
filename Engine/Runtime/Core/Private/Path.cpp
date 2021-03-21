bc81faa84e070c3b2153cfdc9ecfddb1717d22c6
liberty <liberty@clover.games>
2021-03-21 17:05:41 +0900

    Path 클래스 추가

---
+++ Engine/Runtime/Core/Private/Path.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#include "Path.h"
+
+#include <sstream>
+#include "ArgumentNullException.h"
+
+using namespace std;
+
+TRefPtr<String> Path::GetExtension(TRefPtr<String> path)
+{
+	CheckInvalidPathChars(path);
+
+	const int32 length = (int32)path->Length;
+	for (int32 i = length; --i >= 0;)
+	{
+		const wchar_t ch = path[i];
+		// Find first dot that separator of name and extension from last.
+		if (ch == ExtensionSeparatorChar)
+		{
+			if (i != length - 1)
+			{
+				return path->Substring(i);
+			}
+			else
+			{
+				return String::Empty;
+			}
+		}
+
+		// If find character is directory or volume spearator, the path is have not extension.
+		if (ch == DirectorySeparatorChar || ch == AltDirectorySeparatorChar || ch == VolumeSeparatorChar)
+		{
+			break;
+		}
+	}
+
+	return String::Empty;
+}
+
+TRefPtr<String> Path::ChangeExtension(TRefPtr<String> path, TRefPtr<String> extension)
+{
+	CheckInvalidPathChars(path);
+
+	const int32 length = (int32)path->Length;
+	TRefPtr<String> s = path;
+	for (int32 i = length; --i >= 0;)
+	{
+		const wchar_t ch = path[i];
+		// Find first dot that separator of name and extension from last.
+		if (ch == ExtensionSeparatorChar)
+		{
+			s = path->Substring(0, (size_t)i);
+			break;
+		}
+
+		// If find character is directory or volume spearator, the path is have not extension.
+		if (ch == DirectorySeparatorChar || ch == AltDirectorySeparatorChar || ch == VolumeSeparatorChar)
+		{
+			break;
+		}
+	}
+
+	wstringstream wss;
+	wss << s->C_Str;
+	if (extension.IsValid)
+	{
+		// If extension argument is not start with extension separator character.
+		if (extension->Length == 0 || extension[0] != ExtensionSeparatorChar)
+		{
+			wss << ExtensionSeparatorChar;
+		}
+
+		wss << extension->C_Str;
+	}
+
+	return wss.str();
+}
+
+void Path::CheckInvalidPathChars(TRefPtr<String> path, bool bCheckAdditional)
+{
+	if (!path.IsValid)
+	{
+		throw ArgumentNullException(L"path");
+	}
+
+	if (HasIllegalCharacters(path, bCheckAdditional))
+	{
+		throw ArgumentException(L"Invalid path chars.");
+	}
+}
+
+bool Path::HasIllegalCharacters(TRefPtr<String> path, bool bCheckAdditional)
+{
+	return path->IndexOfAny(InvalidPathChars, _countof(InvalidPathChars)) || (bCheckAdditional && AnyPathHasWildCardCharacters(path));
+}
+
+bool Path::AnyPathHasWildCardCharacters(TRefPtr<String> path, int32 startIndex)
+{
+	return path->IndexOfAny(WildCardChars, _countof(WildCardChars)).has_value();
+}
---
+++ Engine/Runtime/Core/Public/Path.h
@@ -0,0 +1,40 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#pragma once
+
+#include "CoreAPI.h"
+#include "Core/Object.h"
+#include "TRefPtr.h"
+
+class String;
+
+class CORE_API Path abstract : virtual public Object
+{
+public:
+	using Super = Object;
+	
+public:
+	static constexpr const wchar_t DirectorySeparatorChar = L'\\';
+	static constexpr const wchar_t AltDirectorySeparatorChar = L'/';
+	static constexpr const wchar_t VolumeSeparatorChar = L':';
+	static constexpr const wchar_t ExtensionSeparatorChar = L'.';
+
+	static constexpr const wchar_t WildCardChars[] = { L'*', L'?' };
+	static constexpr const wchar_t InvalidPathChars[]
+	{
+		L'\"', L'<', L'>', L'|', L'\0',
+		(wchar_t)1, (wchar_t)2, (wchar_t)3, (wchar_t)4, (wchar_t)5, (wchar_t)6, (wchar_t)7, (wchar_t)8, (wchar_t)9, (wchar_t)10,
+		(wchar_t)11, (wchar_t)12, (wchar_t)13, (wchar_t)14, (wchar_t)15, (wchar_t)16, (wchar_t)17, (wchar_t)18, (wchar_t)19, (wchar_t)20,
+		(wchar_t)21, (wchar_t)22, (wchar_t)23, (wchar_t)24, (wchar_t)25, (wchar_t)26, (wchar_t)27, (wchar_t)28, (wchar_t)29, (wchar_t)30,
+		(wchar_t)31
+	};
+
+public:
+	static TRefPtr<String> GetExtension(TRefPtr<String> path);
+	static TRefPtr<String> ChangeExtension(TRefPtr<String> path, TRefPtr<String> extension);
+
+private:
+	static void CheckInvalidPathChars(TRefPtr<String> path, bool bCheckAdditional = false);
+	static bool HasIllegalCharacters(TRefPtr<String> path, bool bCheckAdditional = false);
+	static bool AnyPathHasWildCardCharacters(TRefPtr<String> path, int32 startINdex = 0);
+};
