04c03f9f466bdcdc7c580f264654d3227b4835e5
liberty <liberty@clover.games>
2021-03-29 00:52:36 +0900

    서드파티 프로젝트 업데이트

---
+++ ThirdParty/Source/Zlib/Zlib.Build.cs
@@ -0,0 +1,13 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+public class ZlibBuild : BuildRules
+{
+    public ZlibBuild()
+    {
+        AdditionalPreprocessorDefines.AddRange(new string[]
+        {
+            "_CRT_SECURE_NO_WARNINGS",
+            "_CRT_NONSTDC_NO_WARNINGS"
+        });
+    }
+}
--- generate_projects.bat
+++ generate_projects.bat
@@ -2,33 +2,10 @@
 
 @echo off
 
-cd ThirdParty
-
 echo -
-echo ---------- cmake assimp ----------
+echo ---------- Generate Engine Projects ----------
 echo -
 
-cd assimp
-if exist BINARIES rmdir /s /q BINARIES
-call generate_projects.bat
-cd ..
-
-echo -
-echo ---------- cmake PhysX ----------
-echo -
-
-cd PhysX\physx\
-cd compiler
-if exist vc16win64 rmdir /s /q vc16win64
-cd ..
-call generate_projects.bat
-cd ../..
-
-:: Exit from ThirdParty
-cd ..
-
-call Program\Binaries\GUIDRemover\net5.0\GUIDRemover.exe "ThirdParty"
-call Program\Binaries\GUIDRemover\net5.0\GUIDRemover.exe "Engine"
-call Program\Binaries\GUIDRemover\net5.0\GUIDRemover.exe "Game"
+Binaries\AutomationTool\AutomationTool.exe GenerateEngineSolution .
 
 pause
