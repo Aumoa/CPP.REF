4efc365cafac2b3683d487dfbce192abe63dd5c5
Aumoa <wkdghcjf1234@naver.com>
2021-07-13 20:41:02 +0900

    leak guard 제거

--- Engine/Source/Platform/Windows/WinMain.cpp
+++ Engine/Source/Platform/Windows/WinMain.cpp
@@ -8,13 +8,7 @@
 using namespace std::chrono;
 
 INT __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR cmd, INT show)
-{
-#ifdef _DEBUG
-	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) & ~_CRTDBG_ALLOC_MEM_DF & ~_CRTDBG_LEAK_CHECK_DF);
-	zoned_time leak_guard = system_clock::now();
-	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-	
+{	
 	THGameInstance gameInstance;
 	return gameInstance.Run(gameInstance.CreateSubobject<CoreWindow>());
 }
