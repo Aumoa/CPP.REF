46a01b9a205dd488074bbdbc93f268b8ebbdd1ea
liberty <liberty@clover.games>
2021-12-16 16:32:27 +0900

    로그 출력 포맷 수정

--- Engine/Source/Program/SolutionInterface/Private/LogSolutionInterface.h
+++ Engine/Source/Program/SolutionInterface/Private/LogSolutionInterface.h
@@ -11,8 +11,8 @@ template<class... TArgs>
 void LogAndDisplay(const std::wstring& Message, TArgs&&... InArgs)
 {
 	using namespace std::literals;
-	std::wstring Format = std::format(L"LogSolutionInterface: Verbose: "s + Message, std::forward<TArgs>(InArgs)...);
-	Console::WriteLine(Format);
+	std::wstring Format = std::format(Message, std::forward<TArgs>(InArgs)...);
+	Console::WriteLine(L"LogSolutionInterface: Verbose: "s + Format);
 	SE_LOG(LogSolutionInterface, Verbose, Format);
 }
 
@@ -20,8 +20,8 @@ template<class... TArgs>
 void WarningAndDisplay(const std::wstring& Message, TArgs&&... InArgs)
 {
 	using namespace std::literals;
-	std::wstring Format = std::format(L"LogSolutionInterface: Warning: " + Message, std::forward<TArgs>(InArgs)...);
-	Console::WriteLine(Format);
+	std::wstring Format = std::format(Message, std::forward<TArgs>(InArgs)...);
+	Console::WriteLine(L"LogSolutionInterface: Warning: "s + Format);
 	SE_LOG(LogSolutionInterface, Warning, Format);
 }
 
@@ -29,8 +29,8 @@ template<class... TArgs>
 void ErrorAndDisplay(const std::wstring& Message, TArgs&&... InArgs)
 {
 	using namespace std::literals;
-	std::wstring Format = std::format(L"LogSolutionInterface: Error: "s + Message, std::forward<TArgs>(InArgs)...);
-	Console::WriteLine(Format);
+	std::wstring Format = std::format(Message, std::forward<TArgs>(InArgs)...);
+	Console::WriteLine(L"LogSolutionInterface: Error: "s + Format);
 	SE_LOG(LogSolutionInterface, Error, Format);
 }
 
@@ -38,7 +38,7 @@ template<class... TArgs>
 void FatalAndDisplay(const std::wstring& Message, TArgs&&... InArgs)
 {
 	using namespace std::literals;
-	std::wstring Format = std::format(L"LogSolutionInterface: Fatal: "s + Message, std::forward<TArgs>(InArgs)...);
-	Console::WriteLine(Format);
+	std::wstring Format = std::format(Message, std::forward<TArgs>(InArgs)...);
+	Console::WriteLine(L"LogSolutionInterface: Fatal: "s + Format);
 	SE_LOG(LogSolutionInterface, Fatal, Format);
 }
