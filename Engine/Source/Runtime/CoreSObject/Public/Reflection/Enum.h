da4ce869836f2bd6b4a3bc81b6bf917d4bb41653
Aumoa <wkdghcjf1234@naver.com>
2022-02-19 02:25:48 +0900

    Enum 리플렉션 1차
    - ToString, TryParse 지원

--- Engine/Source/Runtime/CoreSObject/Public/Reflection/Enum.h
+++ Engine/Source/Runtime/CoreSObject/Public/Reflection/Enum.h
@@ -20,9 +20,11 @@ namespace Enum
 		}
 	};
 
-	template<std::integral T, Cstr ConstString>
+	template<class TTag, std::integral T, Cstr ConstString>
 	struct KeyValuePair
 	{
+		using __Tag__ = TTag;
+
 		T Value;
 
 		constexpr KeyValuePair(T&& V) : Value(V)
@@ -35,7 +37,7 @@ namespace Enum
 		}
 	};
 
-	template<std::integral T>
+	template<class TBase, std::integral T>
 	struct EEnumBase
 	{
 		using Type = T;
@@ -61,21 +63,78 @@ namespace Enum
 			return *this;
 		}
 
-		template<Cstr ConstString>
-		using KeyValuePair = KeyValuePair<T, ConstString>;
+		template<class TTag, Cstr ConstString>
+		using MyKeyValuePair = KeyValuePair<TTag, T, ConstString>;
 	};
 }
 
-#define SENUM_DECLARE_ENUM_ELEMENT(Name, Value) Name = Value,
-#define SENUM_DECLARE_CONSTEXPR_ELEMENT(Name, Value) static constexpr KeyValuePair<#Name> Name = (Type)EEnum::Name;
+#define SENUM_DECLARE_ENUM_ELEMENT(Name, ...) Name __VA_OPT__(= __VA_ARGS__),
+#define SENUM_DECLARE_CONSTEXPR_ELEMENT(Name, ...) static constexpr MyKeyValuePair<__Tag__, #Name> Name = (Type)EEnum::Name;
+#define SENUM_DECLARE_DICTIONARY_PAIR_PARSE(Name, ...) { L ## #Name, __Type__(Name) },
+#define SENUM_DECLARE_DICTIONARY_PAIR_TOSTRING(Name, ...) { Name.Value, L ## #Name },
 
 #define SENUM(Name, Type, ...)																		\
-struct Name : public Enum::EEnumBase<Type>															\
+struct Name : public Enum::EEnumBase<Name, Type>													\
 {																									\
+	using __Type__ = Name;																			\
+	struct __Tag__																					\
+	{																								\
+	};																								\
+																									\
 	enum class EEnum : Type																			\
 	{																								\
 		MACRO_RECURSIVE_FOR_EACH_2(SENUM_DECLARE_ENUM_ELEMENT, __VA_ARGS__)							\
 	};																								\
 																									\
 	MACRO_RECURSIVE_FOR_EACH_2(SENUM_DECLARE_CONSTEXPR_ELEMENT, __VA_ARGS__);						\
+																									\
+	Name()																							\
+	{																								\
+	}																								\
+																									\
+	constexpr explicit Name(const Type& Value) : EEnumBase<Name, Type>(Value)						\
+	{																								\
+	}																								\
+																									\
+	template<class TKeyValuePair>																	\
+	constexpr Name(const TKeyValuePair& V) requires													\
+		std::same_as<typename TKeyValuePair::__Tag__, __Tag__>										\
+		: EEnumBase<Name, Type>(V.Value)															\
+	{																								\
+	}																								\
+																									\
+	static bool TryParse(std::wstring_view EnumName, Name& OutValue)								\
+	{																								\
+		static std::map<std::wstring_view, Name, std::less<>> Dictionary =							\
+		{																							\
+			MACRO_RECURSIVE_FOR_EACH_2(SENUM_DECLARE_DICTIONARY_PAIR_PARSE, __VA_ARGS__)			\
+		};																							\
+																									\
+		if (auto It = Dictionary.find(EnumName); It != Dictionary.end())							\
+		{																							\
+			OutValue = It->second;																	\
+			return true;																			\
+		}																							\
+		else																						\
+		{																							\
+			return false;																			\
+		}																							\
+	}																								\
+																									\
+	std::wstring ToString() const																	\
+	{																								\
+		static std::map<Type, std::wstring> Dictionary =											\
+		{																							\
+			MACRO_RECURSIVE_FOR_EACH_2(SENUM_DECLARE_DICTIONARY_PAIR_TOSTRING, __VA_ARGS__)			\
+		};																							\
+																									\
+		if (auto It = Dictionary.find(this->Value); It != Dictionary.end())							\
+		{																							\
+			return It->second;																		\
+		}																							\
+		else																						\
+		{																							\
+			return std::format(L"(" L ## #Name L"){}", this->Value);								\
+		}																							\
+	}																								\
 };
