1c298943016756560d91e0790960799359aa9e7d
Aumoa <wkdghcjf1234@naver.com>
2023-02-20 23:31:28 +0900

    CMake, Ninja 빌드 도구 추가

---
+++ Engine/Build/Ninja/Win64/lib/ninja/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$pp = Get-PackageParameters
+
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ( $pp.InstallDir ) {
+  $installDir = $pp.InstallDir
+}
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile    = Get-Item "$toolsDir\ninja-win*.zip"
+#$zipFile64  = Get-Item "$toolsDir\*-x86_64-pc-windows-gnu*.zip"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $installDir
+  filefullpath  = $zipFile
+  #filefullpath64= $zipFile64
+}
+
+Get-ChocolateyUnzip @packageArgs
+Write-Host "Installed to: '$installDir'"
