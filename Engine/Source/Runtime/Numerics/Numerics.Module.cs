4da5f53df2ef8ee67831bc99069a1ee5b80f4224
liberty <liberty@clover.games>
2023-02-23 13:21:48 +0900

    Numerics Module 분리

---
+++ Engine/Source/Runtime/Numerics/Numerics.Module.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+using System;
+using AE.Rules;
+
+public class Numerics : ModuleRules
+{
+    public Numerics(TargetRules TargetRule) : base(TargetRule)
+    {
+        PublicIncludePaths.Add("Public");
+        PrivateIncludePaths.Add("Private");
+
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core"
+        });
+    }
+}
