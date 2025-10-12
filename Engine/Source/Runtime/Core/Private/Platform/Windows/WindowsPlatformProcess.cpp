dd9dae798fdc6592ee2f1cf74e1db490d5cf4286
liberty <wkdghcjf1234@gmail.com>
2025-10-12 22:19:29 +0900

    Windows에서 로깅 개행 오류 수정

--- Engine/Source/Runtime/Core/Private/Platform/Windows/WindowsPlatformProcess.cpp
+++ Engine/Source/Runtime/Core/Private/Platform/Windows/WindowsPlatformProcess.cpp
@@ -131,7 +131,7 @@ namespace Ayla
 
 	void WindowsPlatformProcess::OutputDebugString(String InMessage) noexcept
 	{
-		::OutputDebugStringW((InMessage + TEXT("\n")).c_str());
+		::OutputDebugStringW(InMessage.c_str());
 	}
 
 	String WindowsPlatformProcess::FindEngineDirectory()
