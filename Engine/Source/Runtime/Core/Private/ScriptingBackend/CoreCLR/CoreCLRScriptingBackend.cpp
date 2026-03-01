75c021fcd97cfd6c06c6396fc1bf46b7f4cbcf8b
liberty <wkdghcjf1234@gmail.com>
2026-03-01 14:36:41 +0900

    [OSX] CoreCLR 지원 추가

--- Engine/Source/Runtime/Core/Private/ScriptingBackend/CoreCLR/CoreCLRScriptingBackend.cpp
+++ Engine/Source/Runtime/Core/Private/ScriptingBackend/CoreCLR/CoreCLRScriptingBackend.cpp
@@ -102,7 +102,7 @@ namespace Ayla
 
 #if PLATFORM_WINDOWS
 		String tpaList_s = String::Join(TEXT(";"), tpaList);
-#elif PLATFORM_LINUX
+#elif PLATFORM_LINUX || PLATFORM_OSX
 		String tpaList_s = String::Join(TEXT(":"), tpaList);
 #else
 #error TODO: Add other platform support.
