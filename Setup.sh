a4ec05e6a0ff22ca1b7559217632ebc603b533e6
liberty <wkdghcjf1234@gmail.com>
2026-03-01 15:26:39 +0900

    [Linux] Setup.sh에 의존성 설치 추가

--- Setup.sh
+++ Setup.sh
@@ -2,5 +2,16 @@
 
 echo Setup
 
+if [[ "$(uname)" == "Linux" ]]; then
+    echo "Installing Linux dependencies..."
+    sudo apt-get update
+    sudo apt-get install -y \
+        libx11-dev \
+        xorg-dev \
+        libxext-dev \
+        libxrandr-dev \
+        libxi-dev
+fi
+
 dotnet build -c Shipping Engine/Source/Programs/AylaBuildTool
 dotnet Engine/Binaries/DotNET/AylaBuildTool.dll build --target "ShaderCompileWorker" --config Development
