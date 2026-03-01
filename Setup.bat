2ad1d0ebff5179ff1de3e3e19036fe5dfc775a9c
liberty <wkdghcjf1234@gmail.com>
2026-03-01 14:12:57 +0900

    Setup 과정에 ShaderCompileWorker 빌드 과정 추가하기

--- Setup.bat
+++ Setup.bat
@@ -4,3 +4,4 @@ ECHO Setup
 
 cd "%~dp0"
 dotnet publish -c Shipping -o Engine\Binaries\DotNET Engine\Source\Programs\AylaBuildTool
+dotnet Engine\Binaries\DotNET\AylaBuildTool.dll build --target "ShaderCompileWorker" --config Development
--- Setup.sh
+++ Setup.sh
@@ -3,3 +3,4 @@
 echo Setup
 
 dotnet build -c Shipping Engine/Source/Programs/AylaBuildTool
+dotnet Engine/Source/Programs/AylaBuildTool.dll build --target "ShaderCompileWorker" --config Development
