76cdd9884bd5b894ec24bf15d506064f8473a5c5
liberty <wkdghcjf1234@gmail.com>
2026-03-01 14:05:59 +0900

    [Unix] ensure 등에서 unused 경고 제거

--- Engine/Source/Runtime/Core/Public/Platform/OSX/OSXPlatformMacros.h
+++ Engine/Source/Runtime/Core/Public/Platform/OSX/OSXPlatformMacros.h
@@ -5,15 +5,6 @@
 #if PLATFORM_OSX
 
 #include "Platform/Generic/GenericPlatformMacros.h"
-#include <signal.h>
-
-#define FORCEINLINE __attribute__((always_inline)) inline
-#define FORCEINLINE_LAMBDA 
-#define FORCENOINLINE __attribute__((noinline))
-#define PLATFORM_BREAK() (::raise(SIGTRAP))
-#define PLATFORM_UNREFERENCED_PARAMETER(X) (void)sizeof((int[]){(X, 0)})
-#define PLATFORM_SHARED_EXPORT __attribute__((visibility("default")))
-#define PLATFORM_SHARED_IMPORT 
-#define PLATFORM_NEWLINE TEXT("\n")
+#include "Platform/Unix/UnixPlatformMacros.h"
 
 #endif
