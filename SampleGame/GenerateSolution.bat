18baaff5385985dc8f57f7e6d88c24c31f0ed11d
liberty <wkdghcjf1234@gmail.com>
2026-04-22 16:55:30 +0900

    Compile Target 추가

---
+++ SampleGame/GenerateSolution.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+ECHO.
+ECHO Generate Solution and Project Files...
+ECHO.
+
+PUSHD "%~dp0"
+  SET BUILDTOOL=Engine\Binaries\DotNET\AylaBuildTool.dll
+  IF NOT EXIST "%BUILDTOOL%" (
+    CALL ..\Setup.bat
+    IF NOT "%errorlevel%"=="0" (
+      EXIT /b %errorlevel%
+    )
+  )
+
+  dotnet "%BUILDTOOL%" generate
+POPD
